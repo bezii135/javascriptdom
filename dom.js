62d46d036de12524be5bf7ac98ab2b6dbc14d0ca
beza <bzendale6@gmail.com>
2024-01-13 15:26:46 +0100

    edit text

--- dom.js
+++ dom.js
@@ -8,10 +8,16 @@ Array.from(titles).forEach(function(item){
 })  */
 /* console.log(Array.isArray(titles));
 console.log(Array.isArray(Array.from(titles))); */
- const wmf = document.querySelector('#book-list li:nth-child(2).name');
+ /* const wmf = document.querySelector('#book-list li:nth-child(2).name');
  //console.log(wmf);
  books =document.querySelectorAll('#book-list li.name');
 Array.from(books).forEach(function(book){
     console.log(book);
-});
- 
+}); */
+var books = document.querySelectorAll('#books-list li.name');
+ Array.from(books).forEach(function(book){
+    book.textContent +='(book title)';
+ });
+ const bookList =document.querySelector('#book-list');
+ bookList.innerHTML='<h2>Books and more...</h2>'
+ bookList.innerHTML += '<p> This is how you add HTML</p>'
