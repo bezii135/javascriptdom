2014aec04a58c65d430a758c38ab489912de9b0f
beza <bzendale6@gmail.com>
2024-01-13 07:12:30 +0100

    class and id

---
+++ dom.js
@@ -0,0 +1,11 @@
+/* var booklist =document.getElementById('book-list');
+booklist; */
+/* var lis = document.getElementsByTagName('li');
+var titles =document.getElementsByClassName('title');
+ 
+Array.from(titles).forEach(function(item){
+    console.log(item);
+})  */
+/* console.log(Array.isArray(titles));
+console.log(Array.isArray(Array.from(titles))); */
+
